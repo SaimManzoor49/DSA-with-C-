9c69e19166be6d1a98ca0b12fca48ae50f793986
unknown <saummanzoor49@gmail.com>
2023-11-08 08:11:12 +0500

    arrays-as-func+call-by-ref

--- Pointers/program.cpp
+++ Pointers/program.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+// void increment(int *a){
+//     *a=*a+1;
+// } 
+
+
+// int SomeOfElements(int a[]) 
+int SomeOfElements(int *a)// same as above
+
+{
+
+cout<<"SomeOfElement function  "<<endl; 
+cout<<" sizeof array a : "<<sizeof(a)<<" sizeof a[0] : "<<sizeof(a[0])<<endl;  // sizeof arr[0] = 4, sizeof arr = 4 -_- (bcz of referance of arr) 
+int size = sizeof(a)/sizeof(a[0]);
+
+int sum=0;
+
+for(int i = 0; i<size;i++){
+    sum+=a[1];
+}
+
+return sum;
+    
+}
+
+
 int main()
 {
 
@@ -46,6 +72,51 @@ int main()
     // int *cp = const_cast<int*>(&c);
     // *cp = 11212; /// OverWriting a const variable using pointers and typeCasting
     // cout<<*cp;
+
+    ///////////////////4} reinterpret cast syntax ->>> reinterpret_cast<type*>(&val) // Most dangerous casting not in cpp standerd used for Pointer to Pointer type casting
+
+
+    ///////////////////////////////////////////// Void Pointer
+    // int a = 10;
+    // void *p = &a;
+
+    // cout<<p<<endl;
+    // cout<<*p<<endl;//not possible bcz the pointer have no type dose not know whats the size of variable in memory(in bytes to look for)
+    // cout<<*p+1<<endl;// same goes for this 
+
+
+    //////////////////////////////////////////////Pointers to Pointers
+
+        // int x=10;
+        // int *p;
+        // p=&x;
+        // *p=20;
+        // int **q=&p;
+        // // cout<<**q<<endl;
+        // int ***ppp = &q;
+        // ***ppp = 30;
+        // cout<<***ppp<<endl;
+
+    /////////////////////////////////////////// Call by referance - useCase of Pointer
+
+    // int a =10;
+    // cout<<a<<endl;
+    // increment(&a);
+    // cout<<a<<endl;
+
+
+    ////////////////////////////////////////// Arrays as function argument
+    int a[] = {1,2,3,4,5};
+    int size = sizeof(a)/sizeof(a[0]); 
+    cout<<"main function  "<<endl;
+    cout<<" sizeof array a : "<<sizeof(a)<<" sizeof a[0]: "<<sizeof(a[0])<<endl;
+
+    int sum = SomeOfElements(a);
+
+    cout<<"sum : "<<sum<<endl;
+
+
+
 
     return 0;
 }
