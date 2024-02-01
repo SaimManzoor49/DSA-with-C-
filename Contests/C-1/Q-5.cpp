7e441bc0457ddfc515c28eda1932bb422b59b2a2
unknown <saummanzoor49@gmail.com>
2024-02-02 00:10:37 +0500

    C1-Q5-fix

--- Contests/C-1/Q-5.cpp
+++ Contests/C-1/Q-5.cpp
@@ -11,21 +11,28 @@ int main(){
     while (t--)
     {
 
-        int n;
-        cin >> n;
-
+        const int n=5;
+        // cin >> n;
         int a[n][n];
         a[0][0] = 1;
 
         for (int i = 1; i < n; ++i)
         {
             a[i][0] = 1;
-            a[i][i-1]=1;
-            for (int j = 1; i < i-1; ++j)
+            a[i][i]=1;
+            for (int j = 1; j < i; ++j)
             {
                 a[i][j] = a[i-1][j-1]+a[i-1][j];
             }
             
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j <= i; ++j)
+            {
+                cout<<a[i][j]<<" ";
+            }
+            cout<<endl;
         }
         
 
